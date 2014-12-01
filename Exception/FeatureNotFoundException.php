96544d6ebe8dbeaef5068e0ac31d08d23358985e
Gustek <panguzol@gmail.com>
2014-12-01 22:39:19 +0000

    FeatureNotFoundException

---
+++ Exception/FeatureNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gustek\FeatureBundle\Exception;
+
+/**
+ * Class FeatureNotFoundException
+ * 
+ * @author Gustek
+ */
+class FeatureNotFoundException extends GustekFeatureBundleException {
+
+} 
---
+++ Exception/GustekFeatureBundleException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gustek\FeatureBundle\Exception;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+/**
+ * Class GustekFeatureBundleException
+ * 
+ * @author Gustek
+ */
+class GustekFeatureBundleException extends Exception {
+
+} 
