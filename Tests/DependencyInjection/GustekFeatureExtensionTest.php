ac5407790360b9a9fe72aa6e5881a4b283c5404c
Gustek <panguzol@gmail.com>
2014-12-01 23:08:18 +0000

    GustekFeatureExtensionTest empty

---
+++ Tests/DependencyInjection/GustekFeatureExtensionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gustek\FeatureBundle\Tests\DependencyInjection;
+
+/**
+ * Class GustekFeatureExtensionTest
+ * 
+ * @author Gustek
+ */
+class GustekFeatureExtensionTest extends \PHPUnit_Framework_TestCase {
+
+}
